834d2d8f7199d1aeccb6243f66b5a26dec72c90a
atulkhot <atul.khot@gmail.com>
2026-01-16 19:03:06 +0530

    First module issues

--- hello.go
+++ hello.go
@@ -1,20 +1,7 @@
-package main
+package hello
 
-import (
-	"fmt"
-	"log"
-)
-import "example.com/greetings"
+import "fmt"
 
-func main() {
-	log.SetPrefix("greetings: ")
-	log.SetFlags(0)
-
-	names := []string{"Gladys", "Samantha", "Darrin"}
-
-	messages, err := greetings.Hellos(names)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(messages)
+func Print() {
+	fmt.Println("Hello, World!")
 }
